0fb0b615a5486137f5b66496bcb534a18444f495
Boyko Amarov <boyko.amarov@gmail.com>
2019-06-18 21:14:09 +0300

    Fix.

--- Problem_2.R
+++ Problem_2.R
@@ -5,9 +5,9 @@ studentId <-
 
 ## Do NOT change the following lines.
 
-originalCars <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/courses%2Feconometrics2019%2Fexam-2019-06-19%2Fcars.csv?alt=media&token=2351e932-24c2-4320-a201-44a64d1f4fbc')
+originalCars <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/courses%2Feconometrics2019%2Fexam-2019-06-19%2Fcars.csv?alt=media&token=2351e932-24c2-4320-a201-44a64d1f4fbc', stringsAsFactors = FALSE)
 cars <- originalCars[sample(1:nrow(originalCars), size = 2000), ]
 
-str(students)
+str(cars)
 
 ## Write your code below this line.
