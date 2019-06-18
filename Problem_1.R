34540f6ce3b6e036ede7f58f4dfe58ac2bfdd6f1
Boyko Amarov <boyko.amarov@gmail.com>
2019-06-18 21:09:52 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,15 @@
+## Problem_1.R
+## Name: 
+## Enter your student Id below and run the line
+studentId <- 
+
+## Do NOT change the following lines.
+
+originalStudents <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/courses%2Feconometrics2019%2Fexam-2019-06-19%2FStudentsPerformance.csv?alt=media&token=3edf2b79-9d0c-4d23-8973-e2c8378f4528')
+students <- originalStudents[sample(1:nrow(originalStudents), size = 300), ]
+
+str(students)
+
+## Write your code below this line.
+
 
